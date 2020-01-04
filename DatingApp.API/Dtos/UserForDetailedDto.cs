e56f8fc60407247ffc034c5842b685b57d2301fb
Tuan Hamid <sl.rahmanhamid@gmail.com>
2020-01-04 06:31:28 +0530

    api users

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,17 +1,14 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
 
         public string Username { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
 
         public string Gender { get; set; }
 
@@ -19,13 +16,13 @@ namespace DatingApp.API.Models
 
         public DateTime Created { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public DateTime LastActive { get; set; }
 
         public string Introduction { get; set; }
 
-        public string  LookingFor { get; set; }
+        public string LookingFor { get; set; }
 
         public string Interests { get; set; }
 
@@ -33,6 +30,9 @@ namespace DatingApp.API.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
+
     }
 }
